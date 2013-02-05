b9bb3b02198036016ae55703fd7472305397cda9
Alexander Jesner <alexander@jesner.eu>
2013-02-05 22:18:50 +0100

    Printing tree for use-def for ret-stmt

--- lib/dataflow/dataflow.cpp
+++ lib/dataflow/dataflow.cpp
@@ -5,14 +5,20 @@
 #include "llvm/Instruction.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/InstrTypes.h"
-
+#include <map>
+#include <set>
 
 using namespace llvm;
+using namespace std;
 
 namespace {
 
+  const unsigned int OPCODE_RET = 1;
+
   struct Dataflow : public FunctionPass {
     static char ID;
+
+    map<Argument*, Value*> argumentTreeHeads;
 
     Dataflow() : FunctionPass(ID) { }
 
@@ -21,7 +27,42 @@ namespace {
       errs().write_escaped(F.getName()) << "\n";
       errs() << "========================================\n";
 
+      printInstructions(F);
+      printDefUseForArguments(F);
 
+      set<Instruction*> returnStatements;
+      findReturnStatements(F, returnStatements);
+      printUseDefForReturnStatements(returnStatements);
+
+      return false;
+    }
+
+    private:
+    void printUseDefForReturnStatements(set<Instruction*>& retStmts) {
+      for (set<Instruction*>::iterator i = retStmts.begin(), e = retStmts.end(); i != e; ++i) {
+        User& u = cast<User>(**i);
+        printOps(u, 0);
+      }
+    }
+
+    void findReturnStatements(Function& F, set<Instruction*>& retStmts) {
+      for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; ++i) {
+          if ((*i).getOpcode() == 1) {
+            retStmts.insert(&*i);
+            errs() << "Fount ret-stmt: " << *i << "\n";
+          }
+      }
+    }
+
+    void printInstructions(Function &F) {
+      errs() << "Instructions: \n";
+
+      for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; ++i) {
+        errs() << (*i) << "\n";
+      }
+    }
+
+    void printDefUseForArguments(Function &F) {
       errs() << "Arg infos: \n";
       Function::arg_iterator a_i = F.arg_begin();
       Function::arg_iterator a_e = F.arg_end();
@@ -29,13 +70,11 @@ namespace {
         Argument& arg = *a_i;
         errs() << arg.getName() << ":\n";
         Value& arg_tree_start = getBaseUserForArgument(arg);
+        argumentTreeHeads.insert(pair<Argument*, Value*>(&arg, &arg_tree_start));
         printUsages(arg_tree_start, 0);
       }
-
-      return false;
     }
 
-    private:
     Value& getBaseUserForArgument(Value& val) {
       // assume val is an argument and always has one user.
       User* user = cast<User>(*(val.use_begin()));
@@ -49,12 +88,21 @@ namespace {
 
       errs() << V << "\n";
 
-      Value::use_iterator i = V.use_begin();
-      Value::use_iterator e = V.use_end();
-      
-      for (; i != e; ++i) {
+      for (Value::use_iterator i = V.use_begin(), e = V.use_end(); i != e; ++i) {
         Value& v = *cast<Value>(*i);
         printUsages(v, level + 1);
+      }
+    }
+
+    void printOps(User &U, int level) {
+      for (int j = 0; j < level; j++)
+        errs() << " ";
+
+      errs() << U << "\n";
+
+      for (User::op_iterator i = U.op_begin(), e = U.op_end(); i != e; ++i) {
+        User& u = *cast<User>(*i);
+        printOps(u, level + 1);
       }
     }
   };
