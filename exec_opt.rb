fe4691e72a846c0f16069c1aade0bfded3435130
Alexander Jesner <alexander@jesner.eu>
2013-02-09 00:49:45 +0100

    Fixed exec_opt file parameter

--- exec_opt.rb
+++ exec_opt.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
-file = ARGV[0]
+file = ARGV[0][0..-2] + "bc"
 puts "\n"
 puts file
 puts "###########################################"
-`opt -load Debug+Asserts/lib/dataflow.so -o /dev/null -loopanalyser -dataflow < #{file}`
+`opt -load Debug+Asserts/lib/dataflow.so -o /dev/null -dataflow < #{file}`
