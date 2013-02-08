a67289fef260b9d922fb63d881e49e010955ed0b
Alexander Jesner <alexander@jesner.eu>
2013-02-08 14:52:53 +0100

    Removed old scripts

--- run_opt.sh
+++
@@ -1,1 +0,0 @@
-opt -load Debug+Asserts/lib/dataflow.so -loopanalyser -dataflow < test/1.bc > /dev/null
--- test/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clang -emit-llvm -S 1.c -o 1.ll
-clang -emit-llvm -c 1.c -o 1.bc
