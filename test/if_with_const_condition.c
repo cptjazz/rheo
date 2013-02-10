7b614c2d0fc3408875a66a1e059388ad9d4f4f05
Alexander Jesner <alexander@jesner.eu>
2013-02-10 09:42:26 +0100

    Merged test file from b7861..

--- test/if_with_const_condition.c
+++ test/if_with_const_condition.c
@@ -1,5 +1,5 @@
-// __expected:bar()
-int bar(int x) {
+// __expected:const_expr()
+int const_expr(int x) {
   char false = 0;
 
   if (false)
@@ -7,3 +7,33 @@ int bar(int x) {
   else
     return 5;
 }
+
+/*
+// __expected:const_expr2()
+int const_expr2(int x) {
+  int a = 1;
+  int b = -1;
+
+  if (a + b)
+    return x;
+  else
+    return 5;
+}
+*/
+/*
+// __expected:const_arith()
+int const_arith(int x) {
+  if (5 - 5)
+    return x;
+  else
+    return 5;
+}
+
+// __expected:const_arith2(x => $_retval)
+int const_arith2(int x) {
+  if (1 * 7 + 2 / 3 + 8)
+    return x;
+  else
+    return 5;
+}
+*/
