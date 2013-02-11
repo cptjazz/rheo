b25702a6913e2b1fb5280f50eee676ef36fb4acc
Alexander Jesner <alexander@jesner.eu>
2013-02-11 21:44:17 +0100

    Added test for while

---
+++ test/while.c
@@ -0,0 +1,45 @@
+// __expected:loop_test_1(a => $_retval, b => $_retval)
+int loop_test_1(int a, int b) {
+  int c;
+  char d;
+  long f;
+
+  d = 9;
+  c = 0;
+  f = 100;
+
+  // a tains retval
+  while (a) {
+    return 6;
+  }
+
+  while (c < 20) {
+    // b taints f and f taints retval
+    f += b;    
+  }
+
+  return f;
+}
+
+// __expected:loop_test_2(a => $_retval, b => $_retval)
+int loop_test_2(int a, int b) {
+  int c;
+  char d;
+  long f;
+
+  d = 9;
+  c = 0;
+  f = 100;
+
+  // a tains retval
+  do {
+    return 6;
+  } while (a);
+
+  do {
+    // b taints f and f taints retval
+    f += b;    
+  } while (c < 20);
+
+  return f;
+}
