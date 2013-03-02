e0d26d4b3230249eea513abcb4801f50e7659c49
Alexander Jesner <alexander@jesner.eu>
2013-03-02 01:52:47 +0100

    Enable passing of opt args to run scripts

--- exec_opt.rb
+++ exec_opt.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/ruby
 raise "No input file specified" if ARGV[0] == nil
+
 file = ARGV[0][0..-2] + "bc"
+opt_args = ""
+opt_args = ARGV[1..ARGV.length].join " " if ARGV.length > 1
+
 puts "\n"
 puts file
 puts "###########################################"
-`opt -load Debug+Asserts/lib/dataflow.so -o /dev/null -mem2reg -instnamer -dataflow < #{file}`
+`opt -load Debug+Asserts/lib/dataflow.so -o /dev/null #{opt_args} -instnamer -dataflow < #{file}`
 
 Dir.glob("*.dot") do |dot|
   `dot -Tpdf #{dot} > #{dot}.pdf`
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ make
 cd test/
 ./compile.rb
 cd ..
-./exec_opt.rb $1
+./exec_opt.rb $1 $2 $3 $4 $5
