eb9fb4037e666a07641b9819b0858152a0039a59
Alexander Jesner <alexander@jesner.eu>
2013-03-02 11:59:00 +0100

    Extended globals test

--- test/globals.c
+++ test/globals.c
@@ -1,4 +1,5 @@
 double PI = 3.1415;
+const int E = 2;
 
 // __expected:nasty_function(a => PI, b => PI)
 void nasty_function(int a, int b) {
@@ -19,4 +20,32 @@ int block_tainted_by_global(int a) {
     return a;
   else
     return 0;
+}
+
+// __expected:const_globals_do_not_taint(a => $_retval)
+int const_globals_do_not_taint(int a) {
+  return a * E;
+}
+
+// __expected:const_globals_do_not_taint_with_pointers(a => b)
+void const_globals_do_not_taint_with_pointers(int a, int* b) {
+  *b = a * E;
+}
+
+// __expected:const_globals_do_not_taint_block(a => $_retval)
+int const_globals_do_not_taint_block(int a) {
+  int sum = 0;
+
+  for (int i = 0; i < E; i++)
+    sum += a;
+ 
+  return sum;
+}
+
+// __expected:const_globals_do_not_taint_block_with_pointers(a => b)
+void const_globals_do_not_taint_block_with_pointers(int a, int* b) {
+  *b = 0;
+
+  for (int i = 0; i < E; i++)
+    *b += a;
 }
