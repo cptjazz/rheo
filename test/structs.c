55dfc0a2bcade49e0b794955e7a30bf3952d6cb1
Alexander Jesner <alexander@jesner.eu>
2013-03-06 16:59:36 +0100

    Added test for structs

---
+++ test/structs.c
@@ -0,0 +1,29 @@
+struct point {
+  int    x;
+  int    y;
+};
+
+typedef struct point point_t;
+typedef unsigned int size_t;
+#define NULL 0
+
+// __expected:point_compare(biggest => $_retval, current => $_retval)
+point_t* point_compare(point_t* biggest, point_t* current) {
+  if (current->x > biggest->x && current->y > biggest->y)
+    return current;
+  else
+    return biggest;
+}
+
+// __expected:biggest_point(size => $_retval, points => $_retval)
+point_t* biggest_point(size_t size, point_t* points)
+{
+  int i;
+  point_t * biggest = NULL;
+
+  for (i = 0; i < size; i++) {
+    biggest = point_compare(biggest, points + i);
+  }
+
+  return biggest;
+}
