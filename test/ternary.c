e1367ee237f817176c110702ee6d68277f172e65
Alexander Jesner <alexander@jesner.eu>
2013-03-11 23:18:14 +0100

    Added test for ternaries

---
+++ test/ternary.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+// __expected:ternary_1(a => $_retval)
+int ternary_1(int a) {
+  int result = a > 5 ? 1 : 0; 
+  return result;
+}
+
+// __expected:ternary_2(a => $_retval)
+int ternary_2(int a) {
+  return rand() > 0.5 ? a : 0; 
+}
