ced8e1edff45d161c3135ccbd4f2b9786b43a09d
Alexander Jesner <alexander@jesner.eu>
2013-03-18 16:02:33 +0100

    Added test for overwriting with globals

---
+++ test/no_transitivity.c
@@ -0,0 +1,41 @@
+int b;
+int c;
+int d;
+
+// __expected:no_transitivity_1(a => b, d => c, d => b)
+int no_transitivity_1(int a) {
+  c = d;
+  b = c;
+  b = a;
+
+  return 0;
+}
+
+// __expected:no_transitivity_2(c => d, b => c, a => b)
+int no_transitivity_2(int a) {
+  d = c;
+  c = b;
+  b = a;
+
+  return 0;
+}
+
+// __expected:no_transitivity_3(b => d, b => c, a => b)
+int no_transitivity_3(int a) {
+  c = b;
+  d = c;
+  b = a;
+
+  return 0;
+}
+
+// __expected:no_transitivity_4(a => d, a => b)
+int no_transitivity_4(int a) {
+  d = a;
+  a = 5;
+  c = a;
+  b = d;
+
+  return 0;
+}
+
