854480409373255b6bbabde1699951e3024f31a8
Alexander Jesner <alexander@jesner.eu>
2013-03-24 12:59:57 +0100

    Implemented ordering via callgraph and circle detection

--- lib/dataflow/dataflow.cpp
+++ lib/dataflow/dataflow.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Module.h"
 #include "llvm/Analysis/Dominators.h"
 #include "llvm/Analysis/PostDominators.h"
+#include "llvm/Analysis/CallGraph.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/InstIterator.h"
 #include "llvm/Instruction.h"
@@ -31,29 +32,69 @@ namespace {
     static char ID;
     queue<Function*> _functionQueue;
     map<Function*, int> _occurrenceCount;
+    map<Function*, Function*> _circularReferences;
+    set<Function*> _queuedFunctionHelper;
 
     Dataflow() : ModulePass(ID) { }
 
     virtual void getAnalysisUsage(AnalysisUsage &AU) const {
       AU.addRequired<PerFunctionFlow>();
+      AU.addRequired<CallGraph>();
+    }
+
+    void findCallGraphChildren(const CallGraphNode* node, const CallGraphNode* startNode) {
+      DEBUG(errs() << "find children: " << node->getFunction()->getName() << " -- " << startNode->getFunction()->getName() << "\n");
+      for (CallGraphNode::const_iterator i = node->begin(), e = node->end(); i != e; ++i) { 
+        const CallGraphNode* kid = i->second;
+
+        if (kid == startNode) {
+          // Found circle.
+
+          _circularReferences.insert(make_pair(node->getFunction(), startNode->getFunction()));
+          continue;
+        }
+
+        if (kid->getFunction())
+          findCallGraphChildren(kid, startNode);
+      }
+
+      Function* f = node->getFunction();
+      addFunctionForProcessing(f);
+    }
+
+    void addFunctionForProcessing(Function* f) {
+      if (!_queuedFunctionHelper.count(f)) {
+        _functionQueue.push(f);
+        _queuedFunctionHelper.insert(f);
+      }
     }
 
     virtual bool runOnModule(Module &module) {
+      CallGraph& CG = getAnalysis<CallGraph>();
+
       for (Module::iterator i = module.begin(), e = module.end(); i != e; ++i) {
         Function* f = &*i;
-        
+
         // Skip if function was already processed.
         if (TaintFile::exists(*f))
           continue;
 
-        _functionQueue.push(f);
+        const CallGraphNode* node = CG[f];
+
+        findCallGraphChildren(node, node);
+        addFunctionForProcessing(f);
+        
         _occurrenceCount.insert(pair<Function*, int>(f, 1));
+      }
+
+      for (map<Function*, Function*>::iterator f_i = _circularReferences.begin(), f_e = _circularReferences.end(); f_i != f_e; ++f_i) {
+        errs() << "Founc circ-ref: " << f_i->first->getName() << " <--> " << f_i->second->getName() << "\n";
       }
 
       while (!_functionQueue.empty()) {
         Function* f = _functionQueue.front();
 
-        if (_occurrenceCount[f]++ > 10) {
+        if (_occurrenceCount[f]++ > 3) {
           errs() << "__error:PANIC: detected endless loop. Aborting.\n";
           return false;
         }
