ea6b5630adfd47ab0d4e1cf098069102b6ec363a
Alexander Jesner <alexander@jesner.eu>
2013-04-22 19:51:29 +0200

    Added test file for function pointers

--- test/function_pointers.c
+++ test/function_pointers.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 int (*operation)(int x, int y);
 
 // __expected:op1(x => $_retval, y => $_retval)
@@ -25,4 +25,32 @@ int variable_operation(int foo, int bar, int fu, int baz)
   int y = operation(fu, baz);
 
   return x + y;
+}
+*/
+// __expected:op3(x => $_retval)
+int op3(int x, int y)
+{
+  return x;
+}
+
+// __expected:op4(y => $_retval)
+int op4(int x, int y)
+{
+  return y;
+}
+
+// __expected:variable_operation_2(fswitch => $_retval, fu => $_retval, baz => $_retval)
+int variable_operation_2(int fswitch, int fu, int baz)
+{
+  int (*operation2)(int x, int y);
+
+  if (fswitch) {
+    operation2 = op3;
+  } else {
+    operation2 = op4;
+  }
+
+  int y = operation2(fu, baz);
+
+  return y;
 }
