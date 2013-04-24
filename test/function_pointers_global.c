d3f81870145aff02a6d2071a942a3af9ffe11684
Alexander Jesner <alexander@jesner.eu>
2013-04-24 21:40:25 +0200

    Added test for global function pointer aliasing

---
+++ test/function_pointers_global.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int (*operation)(int x, int y);
+
+// __expected:op1(x => $_retval)
+int op1(int x, int y)
+{
+  return x;
+}
+
+// __expected:op2(y => $_retval)
+int op2(int x, int y)
+{
+  return y;
+}
+
+
+// __expected:set_to_op2()
+void set_to_op2()
+{
+  operation = op2;
+}
+
+// __expected:variable_operation_global(fu => $_retval, baz => $_retval)
+int variable_operation_global(int fu, int baz)
+{
+  operation = op1;
+
+  // After this line, operation is op2
+  set_to_op2();
+
+  int y = operation(fu, baz);
+  return y;
+}
+
+// __expected:variable_operation_global_2(fu => $_retval, baz => $_retval)
+int variable_operation_global_2(int fu, int baz)
+{
+  operation = op1;
+
+  // After this line, operation is op2
+  // set_to_op2();
+  // even without calling this, we should find the function pointer
+  // alias generated in the functino. #conservatism
+
+  int y = operation(fu, baz);
+  return y;
+}
