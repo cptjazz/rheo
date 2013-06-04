c4c067ca962f7dadb7c8ffdd5be468cfac7afa0a
Alexander Jesner <alexander@jesner.eu>
2013-06-04 08:59:12 +0200

    Added new test for tricky flow via globals

---
+++ test/globals_hidden_flow.c
@@ -0,0 +1,35 @@
+int global_option, g2;
+
+// __expected:m1(global_option => g2)
+void m1() {
+  if (global_option) {
+    g2 = 5;
+  }
+}
+
+
+// __expected:hidden_flow(local_option => $_retval, g2 => $_retval)
+int hidden_flow(int local_option, int b) {
+  // local_option => global_option
+  global_option = local_option;
+
+  // Taint is lost on 'local_option' and 'b'
+  local_option = 5;
+  b = local_option;
+
+  // local_option => global_option => g2
+  m1();
+
+  // local_option =/> global_option
+  global_option = 99;
+
+  // local_option => g2 => b
+  if (g2)
+    b = 1;
+
+  // local_option =/> g2
+  g2 = 88;
+
+  // b => ret
+  return b;
+}
