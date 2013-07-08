69e2535b7727d2a2f3c31e2005d0209aca978bbd
Daniel Hein <dhein@iaik.tugraz.at>
2013-07-08 11:48:35 +0200

    another nice loop test

---
+++ test/while0003.c
@@ -0,0 +1,40 @@
+//============================================================================
+//
+// ---=== while0003.c ===---
+// Tests information flow in while loop. Specifically constructed to prevent
+// compiler from figuring out what the loop does. Consequently, even "works"
+// with compiler optimization (-O2) turned on.
+//
+// ---------------------------------------------------------------------------
+// Flags
+// OL = 2
+//============================================================================
+
+#include <stdio.h>
+#include <limits.h>
+
+int cpy = 23;
+int global_base_ = 0;
+
+// __expected:copy(base => global_base_, a => global_base_, base => $_retval)
+int copy(int base, int a) {
+  //int i = INT_MIN;
+  while (base < a) {
+    base -= 1;
+    global_base_++;
+  }
+  return base;
+}
+
+// __expected:main()
+int main() {
+  int base = 0;
+  // __define:scanf()
+  scanf("Enter base: %d\n", &base);
+  global_base_ = base;
+  int cpyd = copy(base, cpy);
+  // __define:printf(1 => 0, 2 => 0)
+  printf("copy(%d)=%d\n", cpy, cpyd);
+  return 0;
+}
+
