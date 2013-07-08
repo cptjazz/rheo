6889448c5cbdb3a912653f273220bb2e205c45a6
Daniel Hein <dhein@iaik.tugraz.at>
2013-07-08 11:28:57 +0200

    Added example for new self-tainting proposed by Roderick

---
+++ test/self_taint.c
@@ -0,0 +1,24 @@
+// __expected:f_1()
+void f_1(int* a, int* b) {
+}
+
+// __expected:g_1(a => $_retval)
+int g_1(int a, int b) {
+  f_1(&a, &b);
+  return a;
+} 
+
+// __expected:f_2()
+void f_2(int* a, int* b) {
+  *a = 0;
+  *b = 0;
+}
+
+
+// __expected:g_2(a => $_retval)
+int g_2(int a, int b) {
+  f_2(&a, &b);
+  return a;
+}
+
+ 
