b0f0223f43240c205a022f4ef521c9d1ec24edf5
Alexander Jesner <alexander@jesner.eu>
2013-07-19 10:06:58 +0200

    Added test for varargs+recursion -- currently failing due to bug in analysis.

---
+++ test/varargs_recursion.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+double call_avg(int a, int b, int c);
+double average(int count, ...);
+
+// __expected:call_avg(a => $_retval, b => $_retval, c => $_retval)
+double call_avg(int a, int b, int c) {
+  return average(5, a, 4, b, 8, c);
+}
+
+// __expected:average(count => $_retval, ... => $_retval, count => ..., ... => ...)
+double average(int count, ...)
+{
+  va_list ap;
+  int j;
+  double tot = 0;
+
+  va_start(ap, count); //Requires the last fixed parameter (to get the address)
+
+  for(j=0; j<count; j++)
+    tot += va_arg(ap, double); //Requires the type to cast to. Increments ap to the next argument.
+
+  va_end(ap);
+
+  return tot / call_avg(j, count, 2);
+}
