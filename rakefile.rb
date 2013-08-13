50e7466e68cf22c6c48fffb99e6823dfc4194aa1
Alexander Jesner <alexander@jesner.eu>
2013-08-13 15:53:26 +0200

    Force 32-bit compile and use -instcombine to remove redundant operations and get rid of bitcasted calls (happen when merging bc-files with different architecture (x32, x64)). Use -dse to remove useless store instructions.

--- rakefile.rb
+++ rakefile.rb
@@ -3,14 +3,7 @@ require 'rainbow'
 require 'rake/clean'
 require 'pty'
 
-OPTS = ["", "-mem2reg", "-scalarrepl", "-reg2mem", "-mem2reg -reg2mem", "-adce", "-mem2reg -adce", "-licm", "-constmerge", "-constprop", "-globaldce", "-dse", "-deadargelim", "-mergereturn", "-sink", "-loop-unroll", "-loop-simplify", "-sccp", "-lowerswitch", "-reassociate", "-tailcallelim"]
-
-@opts = []
-1.times do |i| 
-  @opts << OPTS.combination(i + 1).to_a.map { |x| x.join(" ") }
-end
-
-@opts.flatten!
+@opt_args = "-globalopt -globaldce -instcombine -tailcallelim -licm -sink -adce -dse -mem2reg -scalarrepl -dataflow"
 
 desc "Run all tests, but only show failed ones"
 namespace :test do
@@ -54,7 +47,7 @@ def analyse(args)
     log_file = File.open("analysis.log", "w")
     puts " * Preprocessing BC file"
 
-    opt_cmd = "opt -load ../Release+Asserts/lib/dataflow.so -globalopt -globaldce -tailcallelim -licm -sink -adce -scalarrepl -dataflow < #{file} -o /dev/null 2>&1"
+    opt_cmd = "opt -load ../Release+Asserts/lib/dataflow.so #{@opt_args} < #{file} -o /dev/null 2>&1"
     begin
       PTY.spawn(opt_cmd) do |r, w, pid|
         begin
@@ -155,28 +148,25 @@ def run_tests(show_only, args)
       exp_file.scan(/__define:(.+)\((.*)\)/) { |m| def_map[m[0]] = m[1].split(', ') }
 
       `clang -emit-llvm -c #{file}.c -o #{file}.bc`
+      `rm *.taints 2>/dev/null`
+      `rm *.taints.temp 2>/dev/null`
 
-      @opts.each do |opt|
-        `rm *.taints 2>/dev/null`
-        `rm *.taints.temp 2>/dev/null`
-        create_taint_file(def_map)
+      create_taint_file(def_map)
+      out_map = {}
 
-        out_map = {}
+      opt_out = `opt -load ../Release+Asserts/lib/dataflow.so #{@opt_args} < #{file}.bc -o /dev/null 2>&1`
 
-        opt_out = `opt -load ../Release+Asserts/lib/dataflow.so #{opt} -debug -globalopt -scalarrepl -instnamer -dataflow < #{file}.bc -o /dev/null 2>&1`
-        
-        opt_out.scan(/__taints:(.+)\((.*)\)/) { |m| out_map[m[0]] = m[1].split(', ') }
+      opt_out.scan(/__taints:(.+)\((.*)\)/) { |m| out_map[m[0]] = m[1].split(', ') }
 
-        File.open(file + "#{opt}.log", "w") do |logfile|
-          logfile.puts opt_out
-        end
-
-        (test_result, passed_count, failed_count) = test(file, exp_map, out_map, opt, show_only)
-
-        overall_passed += passed_count
-        overall_failed += failed_count
-        overall_result &&= test_result
+      File.open(file + ".log", "w") do |logfile|
+        logfile.puts opt_out
       end
+
+      (test_result, passed_count, failed_count) = test(file, exp_map, out_map, show_only)
+
+      overall_passed += passed_count
+      overall_failed += failed_count
+      overall_result &&= test_result
     end
 
     puts
@@ -188,13 +178,13 @@ def run_tests(show_only, args)
   end
 end
 
-def test(file, exp_map, out_map, opts, show_only)
+def test(file, exp_map, out_map, show_only)
   test_result = true
   failed_count = 0
   passed_count = 0
 
   unless exp_map.length == out_map.length
-    print_failed(file, "", "Function count mismatch. Expected #{exp_map.length} but was #{out_map.length}", opts)
+    print_failed(file, "", "Function count mismatch. Expected #{exp_map.length} but was #{out_map.length}")
     puts exp_map.keys
     puts " --- But was ---".color(:yellow)
     puts out_map.keys
@@ -203,7 +193,7 @@ def test(file, exp_map, out_map, opts, show_only)
   
   exp_map.sort.each do |function, taints|
     unless out_map.has_key? function
-      print_failed(file, function, "function `#{function}` not found", opts)
+      print_failed(file, function, "function `#{function}` not found")
       test_result &&= false
       failed_count += 1
       next
@@ -211,7 +201,7 @@ def test(file, exp_map, out_map, opts, show_only)
 
     out_taints = out_map[function]
     unless taints.length == out_taints.length
-      print_failed(file, function, "Taint count mismatch. Expected #{taints.length} but was #{out_taints.length}", opts)
+      print_failed(file, function, "Taint count mismatch. Expected #{taints.length} but was #{out_taints.length}")
       puts " --- Expected ---".color(:yellow)
       puts taints.sort
       puts " --- But was ---".color(:yellow)
@@ -224,7 +214,7 @@ def test(file, exp_map, out_map, opts, show_only)
     single_taint_check_result = true
     taints.each do |taint|
       unless out_taints.include? taint
-        print_failed(file, function, "Expected taint missing: " + taint.color(:cyan), opts)
+        print_failed(file, function, "Expected taint missing: " + taint.color(:cyan))
         test_result &&= false
         failed_count += 1
         single_taint_check_result = false
@@ -233,7 +223,7 @@ def test(file, exp_map, out_map, opts, show_only)
   
     next unless single_taint_check_result
 
-    print_passed(file, function, opts) if show_only == :all
+    print_passed(file, function) if show_only == :all
     passed_count += 1
   end
 
@@ -248,18 +238,16 @@ def create_taint_file(def_map)
   end
 end
 
-def print_failed(file, function, reason, opts)
+def print_failed(file, function, reason)
   print " * " + "FAILED".bright.color(:red)
-  print " [#{opts}]".color("#AAAAAA")
   print " : " + file.color(:blue) 
   print "." + function.italic if function.length > 0
   print " -- " + reason.color(:yellow)
   print "\n"
 end
 
-def print_passed(file, function, opts)
+def print_passed(file, function)
   print " * " + "PASSED".bright.color(:green)
-  print " [#{opts}]".color("#AAAAAA")
   print " : " + file.color(:blue) 
   print "." + function.italic
   print "\n"
