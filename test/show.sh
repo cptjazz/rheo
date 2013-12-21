1d503a48ca3d916f1a75a4bb01c4700094c7e663
Alexander Jesner <alexander@jesner.eu>
2013-12-21 15:12:25 +0100

    Updated show-script

--- test/show.sh
+++ test/show.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-opt -strip-debug-declare -instcombine -constmerge -constprop -tailcallelim -licm -sink -adce -dse -mem2reg -scalarrepl -globalopt -globaldce -die -instnamer -simplifycfg $1 | llvm-dis | less
+opt -strip-debug -instcombine -constmerge -constprop -tailcallelim -licm -sink -adce -dse -mem2reg -scalarrepl -globalopt -globaldce -die -instnamer -simplifycfg $1 | llvm-dis | less
